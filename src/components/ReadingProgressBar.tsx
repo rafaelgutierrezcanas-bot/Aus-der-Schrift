ad5c3bbf15b064eafcb2362a5e12ab60b75a2b26
Rafael <rafaelgutierrezcanas@gmail.com>
2026-06-29 21:59:00 +0200

    fix: move reading progress bar back to top: 0 (was cutting through content at 64px)

--- src/components/ReadingProgressBar.tsx
+++ src/components/ReadingProgressBar.tsx
@@ -22,7 +22,7 @@ export function ReadingProgressBar() {
     <div
       style={{
         position: "fixed",
-        top: "64px",
+        top: 0,
         left: 0,
         width: "100%",
         height: "3px",
